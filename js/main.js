cc23bff8dc360c215f0ba837b9dc971527ea1db7
Jez Cowley <jez@carboncreative.net>
2020-08-22 12:01:34 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 window.onload = () => {
     'use strict';
-  
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
         .register('./serviceWorker.js');
     }
-    // Detects if device is on iOS 
-    const isIos = () => {
-        const userAgent = window.navigator.userAgent.toLowerCase();
-        return /iphone|ipad|ipod/.test( userAgent );
-    }
-    // Detects if device is in standalone mode
-    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-    
-    // Checks if should display install popup notification:
-    if (isIos() && !isInStandaloneMode()) {
-        this.setState({ showInstallMessage: true });
-    }
+
+    let divInstall = document.querySelector('#installBanner');
+
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+      console.log('👍', 'beforeinstallprompt', event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container
+      divInstall.classList.toggle('hidden', false);
+    });
+
+
+    // window.addEventListener('DOMContentLoaded', () => {
+    //   let displayMode = 'browser tab';
+    //   if (navigator.standalone) {
+    //     displayMode = 'standalone-ios';
+    //   }
+    //   if (window.matchMedia('(display-mode: standalone)').matches) {
+    //     displayMode = 'standalone';
+    //   }
+    //   // Log launch display mode to analytics
+    //   console.log('DISPLAY_MODE_LAUNCH:', displayMode);
+    // });
+
   }
+
